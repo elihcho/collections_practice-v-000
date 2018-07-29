0676495b4e9b213d821fc711dfb30c68c63417ba
Eli Cho <eli.cho@gmail.com>
2018-07-29 05:10:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,5 +20,7 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  array.each {|word| word[2] = "$"}
+  new_array = []
+  new_array << array.each {|word| word[2] = "$"}
+  return new_array
 end
